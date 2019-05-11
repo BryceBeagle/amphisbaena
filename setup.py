1fe9f0b2a3c664f7431946cb745927db6ba7c235
ignormies <bryce.beagle@gmail.com>
2019-05-11 13:37:26 -0700

    Add zip_safe=False to prevent setup.py warning

--- setup.py
+++ setup.py
@@ -21,4 +21,6 @@ setup(name='amphisbaena',
                             compiler_directives={'language_level': "3"},
                             gdb_debug=True),
       requires=['Cython', 'numpy'],
-      include_dirs=[numpy.get_include()])
+      include_dirs=[numpy.get_include()],
+      # https://stackoverflow.com/a/10618900/8134178
+      zip_safe=False)
