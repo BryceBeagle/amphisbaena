1d09362e468c3e018c813dcb63a1f410cc561d5f
ignormies <bryce.beagle@gmail.com>
2019-05-11 13:40:03 -0700

    Initial commit outlining all the different types of instructions

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from amphisbaena.rom import rom
+from amphisbaena.rom import rom, instruction
 
 rom_obj = rom.ROM("roms/super_mario_advance.gba")
 print(rom_obj.rom_header)
+
+instr = instruction.BaseInstruction()
