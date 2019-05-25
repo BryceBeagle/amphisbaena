5a67fb7fd355fdd4d43016506e197e5d45d8c78c
ignormies <bryce.beagle@gmail.com>
2019-05-25 15:56:12 -0700

    Remove class around tests for now. Not being used

--- tests/instruction/test_base_instruction.py
+++ tests/instruction/test_base_instruction.py
@@ -3,52 +3,51 @@ from amphisbaena.instruction.base_instruction import Instruction
 from amphisbaena.instruction.base_instruction import Condition
 
 
-class TestInstruction:
-    def test_condition(self):
-        inst = 0b00000000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.EQ
+def test_condition():
+    inst = 0b00000000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.EQ
 
-        inst = 0b00010000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.NE
+    inst = 0b00010000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.NE
 
-        inst = 0b00100000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.CS
+    inst = 0b00100000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.CS
 
-        inst = 0b00110000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.CC
+    inst = 0b00110000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.CC
 
-        inst = 0b01000000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.MI
+    inst = 0b01000000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.MI
 
-        inst = 0b01010000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.PL
+    inst = 0b01010000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.PL
 
-        inst = 0b01100000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.VS
+    inst = 0b01100000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.VS
 
-        inst = 0b01110000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.VC
+    inst = 0b01110000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.VC
 
-        inst = 0b10000000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.HI
+    inst = 0b10000000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.HI
 
-        inst = 0b10010000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.LS
+    inst = 0b10010000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.LS
 
-        inst = 0b10100000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.GE
+    inst = 0b10100000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.GE
 
-        inst = 0b10110000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.LT
+    inst = 0b10110000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.LT
 
-        inst = 0b11000000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.GT
+    inst = 0b11000000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.GT
 
-        inst = 0b11010000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.LE
+    inst = 0b11010000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.LE
 
-        inst = 0b11100000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.AL
+    inst = 0b11100000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.AL
 
-        inst = 0b11110000_00000000_00000000_00000000
-        assert Instruction(inst).condition == Condition.RESERVED
+    inst = 0b11110000_00000000_00000000_00000000
+    assert Instruction(inst).condition == Condition.RESERVED
