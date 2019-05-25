1e1fe494b35cceb10bf0a8db8a9e50bb1349d119
ignormies <bryce.beagle@gmail.com>
2019-05-25 14:14:39 -0700

    Allow clean step to work even if the cython code does not compile

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ import os
 # setuptools import MUST be above cythonize import
 from setuptools import setup
 from Cython.Build import cythonize
+from Cython.Distutils.build_ext import build_ext
 import numpy
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "WARN"))
@@ -20,7 +21,7 @@ class CleanCommand(clean):
     def run(self):
         super().run()
 
-        for ext in [".so", ".c", ".html"]:
+        for ext in [".so", ".c", ".h", ".html"]:
             files = Path("amphisbaena").glob(f"**/*{ext}")
             if files:
                 log.info(f"removing {ext} build files")
@@ -33,16 +34,32 @@ class CleanCommand(clean):
             os.remove(file)
 
 
+class BuildExtCommand(build_ext):
+    """This exists so that `setup.py clean` can be run even if the cython does
+    not build
+
+    cythonize() is called only when `build_ext` is actually used
+    """
+
+    def __init__(self, dist):
+        dist.ext_modules = cythonize(
+            "**/*.pyx",
+            compiler_directives={'language_level': "3"},
+            gdb_debug=True,
+            annotate=True)
+
+        super().__init__(dist)
+
+
 setup(name='amphisbaena',
       packages=["amphisbaena",
                 "amphisbaena.instruction",
                 "amphisbaena.rom"],
-      ext_modules=cythonize("**/*.pyx",
-                            compiler_directives={'language_level': "3"},
-                            gdb_debug=True, annotate=True),
+      ext_modules=[],
       requires=['Cython', 'numpy'],
       include_dirs=['.', numpy.get_include()],
       zip_safe=False,
       cmdclass={
+          'build_ext': BuildExtCommand,
           'clean': CleanCommand
       })
