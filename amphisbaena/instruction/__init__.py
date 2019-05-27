fcff6af084d9cf143a0b5489775d95280a5814ec
ignormies <bryce.beagle@gmail.com>
2019-05-26 22:12:48 -0700

    Branch and exchange instruction

--- amphisbaena/instruction/__init__.py
+++ amphisbaena/instruction/__init__.py
@@ -1,5 +1,7 @@
 from .base_instruction import Instruction
 # noinspection PyUnresolvedReferences
 from .base_instruction import Condition, Register
+
+from .branch_and_exchange_instruction import BranchAndExchangeInstruction
 from .data_processing_instruction import DataProcessingInstruction
 from .multiply_instruction import MultiplyInstruction
